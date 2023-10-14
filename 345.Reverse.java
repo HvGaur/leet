f56d91d1fdbaa905b8b4616318577a8c8b2d5b77
HvGaur <hvgaurr@gmail.com>
2023-10-14 05:46:21 +0530

    345

--- 345.Reverse.java
+++ 345.Reverse.java
@@ -1,12 +1,13 @@
 class Solution {
   public String reverseVowels(String s) {
-    
+
     final String kVowels = "aeiouAEIOU";
     StringBuilder sb = new StringBuilder(s);
     int l = 0;
     int r = s.length() - 1;
 
-    while (l < r) {
+    while (l < r) 
+    {
       while (l < r && !kVowels.contains("" + sb.charAt(l)))
         ++l;
       while (l < r && !kVowels.contains("" + sb.charAt(r)))
