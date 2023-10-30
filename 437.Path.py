40d48c9e5fca5f41321777313f82217034602496
HvGaur <hvgaurr@gmail.com>
2023-10-30 22:07:21 +0530

    437

---
+++ 437.Path.py
@@ -0,0 +1,34 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def pathSum(self, root, targetSum):
+        stack = [root]
+        self.count = 0
+        self.targetSum = targetSum
+        
+        # iterate over all the nodes
+        while(stack):
+            top = stack.pop()
+            if top:
+                stack.insert(0, top.left)
+                stack.insert(0, top.right)
+            
+            # traverse the tree using each node as root
+            self.traverseTree(top, 0)
+
+        return self.count
+
+    def traverseTree(self, curr, summ):
+        if not curr:
+            return
+
+        summ += curr.val
+        if summ == self.targetSum:
+            self.count += 1
+
+        self.traverseTree(curr.left, summ)
+        self.traverseTree(curr.right, summ)
