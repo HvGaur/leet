88f79095f40d7d78e1376022bac358900e64fdb1
HvGaur <hvgaurr@gmail.com>
2023-12-12 14:27:03 +0530

    p

--- pythonclass.py
+++ pythonclass.py
@@ -1,3 +1,6 @@
-x = input('Enter the name:')
-print("hello ", x)
-y = 
+x = input('Enter the name:\n')
+y = input("Enter your birth year:\n")
+if 2023-int(y) > 18:
+    print("You're above 18\n")
+else:
+    print("You're a minor\n")
